d4ca156741d182fccad805acd65845dd522b9bfe
Thomas-Longworth <>
2023-07-23 16:28:41 +0000

    add products.html

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 # Create your views here.
+
+
+def all_products(request):
+    products = Product.objects.all()
+    context = {
+        'products':products,
+    }
+
+
+    return render(request, 'products.html', context)
