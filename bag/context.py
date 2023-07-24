25ec025c0ba671771bdfa5cf8447c282aebbe1f7
Thomas-Longworth <>
2023-07-24 16:18:56 +0000

    add cart functionality

--- bag/context.py
+++
@@ -1,8 +0,0 @@
-from decimal import Decimal
-from django.conf import settings
-
-
-def bag_contents(request):
-
-    context = {}
-    return context
--- bag/views.py
+++ bag/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 def view_bag(request):
     return render(request, 'bag.html')
+
+
+def add_to_bag(request, item_id):
+    
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
+
+    
