0cfbe5999e31a19bff74f6d6cc5bf08d58cf726f
Thomas-Longworth <>
2023-07-25 15:51:50 +0000

    add toast container for messages

--- bag/views.py
+++ bag/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect
-  
-# Create your views here.
+from django.contrib import messages
+from products.models import Product
+
+
 def view_bag(request):
+
     return render(request, 'bag.html')
 
 
 def add_to_bag(request, item_id):
-    
+
+    product = Product.objects.get(pk=item_id)
+   
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -14,11 +19,11 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Added {product.name} to your bag')
     else:
         bag[item_id] = quantity
+      
 
     request.session['bag'] = bag
     print(request.session['bag'])
     return redirect(redirect_url)
-
-    
