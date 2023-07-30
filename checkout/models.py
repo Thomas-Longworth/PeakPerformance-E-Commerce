f8c197706bded6e8c28dad6152a305f2727db96a
Thomas-Longworth <>
2023-07-30 20:27:49 +0000

    add carousel to main page

--- checkout/models.py
+++ checkout/models.py
@@ -39,7 +39,7 @@ class Order(models.Model):
         accounting for delivery costs.
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))[
-            'lineitem_total__sum']
+            'lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * \
                 settings.STANDARD_DELIVERY_PERCENTAGE / 100
@@ -66,13 +66,13 @@ class OrderLineItem(models.Model):
                               on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(
         Product, null=False, blank=False, on_delete=models.CASCADE)
-   
+
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
-     
+
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
