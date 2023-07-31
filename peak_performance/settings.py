39cf3558974b2b4e11390d8377aba05e7e94e04b
Thomas-Longworth <>
2023-07-31 19:25:24 +0000

    remove email requirements for registrations

--- peak_performance/settings.py
+++ peak_performance/settings.py
@@ -109,9 +109,8 @@ WSGI_APPLICATION = 'peak_performance.wsgi.application'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
-ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_EMAIL_REQUIRED = False
+
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
