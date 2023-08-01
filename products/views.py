27bc0d44ad15b3648ca71b0e5a97917e8052ecdb
Thomas-Longworth <>
2023-08-01 14:57:09 +0000

    add product form to add_product.html

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Product, Category
 from django.db.models import Q
-
+from .forms import ProductForm
 # Create your views here.
 
 
@@ -32,3 +32,14 @@ def product_detail(request, product_id):
         'product': product,
     }
     return render(request, 'product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
