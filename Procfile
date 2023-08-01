4942f70a50c5706e1e1d499ea66adad126904ff4
Thomas-Longworth <>
2023-08-01 19:27:05 +0000

    deploy attempt

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: gunicorn peak_performanceher.wsgi:application
+web: gunicorn peak_performance.wsgi:application
+
+
--- peak_performance/settings.py
+++ peak_performance/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-thomas-longworth-peakper-uphedgsn5e.us2.codeanyapp.com', 'peak-perfomance.herokuapp.com']
+    '8000-thomas-longworth-peakper-uphedgsn5e.us2.codeanyapp.com', 'peak-perfomance-2c33a326e9fb.herokuapp.com']
 
 
 # Application definition
