e3f87079c450706904cce055580a653d2cd9b29f
Thomas-Longworth <>
2023-08-02 15:06:44 +0000

    test deploy

--- peak_performance/settings.py
+++ peak_performance/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-thomas-longworth-peakper-uphedgsn5e.us2.codeanyapp.com', 'peak-perfomance-2c33a326e9fb.herokuapp.com', 'localhost', 'peak-performance.herokuapp.com']
