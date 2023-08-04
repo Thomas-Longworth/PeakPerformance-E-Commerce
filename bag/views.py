942922822d29348361e790b62988475b725ec130
Thomas-Longworth <>
2023-08-04 18:53:39 +0000

    fix bug with bag sessions

--- bag/views.py
+++ bag/views.py
@@ -11,7 +11,6 @@ def view_bag(request):
 def add_to_bag(request, item_id):
 
     product = Product.objects.get(pk=item_id)
-   
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -22,8 +21,47 @@ def add_to_bag(request, item_id):
         messages.success(request, f'Added {product.name} to your bag')
     else:
         bag[item_id] = quantity
-      
 
     request.session['bag'] = bag
     print(request.session['bag'])
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
+    bag = request.session.get('bag', {})
+
+    if request.method == "POST":
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect('view_bag')
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
