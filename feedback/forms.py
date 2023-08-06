d66d8c12bb20417ff28a371f5d0c388f3f80873e
Thomas-Longworth <>
2023-08-06 17:46:37 +0000

    fix issue with customer feedback dates

--- feedback/forms.py
+++ feedback/forms.py
@@ -1,9 +1,18 @@
 from django import forms
 from .models import UserFeedback
 
+from django.forms import ModelForm
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 
 class feedbackForm(forms.ModelForm):
     class Meta:
         model = UserFeedback
-        fields = ['name', 'today_date', 'category', 'product',
-                  'rating', 'purchase_date', 'description']
+        fields = ['name', 'made_on', 'category', 'product',
+                  'rating', 'description']
+        widgets = {
+            'made_on': DateInput(),
+        }
