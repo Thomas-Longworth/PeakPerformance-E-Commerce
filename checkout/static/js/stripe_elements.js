4c1b4e9788e9b764a8a52dba4ae8b0b279c25193
Thomas-Longworth <>
2023-08-08 17:09:17 +0000

    fix issue with stripe_elements.js not loading

--- checkout/static/js/stripe_elements.js
+++ checkout/static/js/stripe_elements.js
@@ -28,6 +28,7 @@ var style = {
 var card = elements.create('card', { style: style });
 card.mount('#card-element');
 
+// Handle realtime validation errors on the card element
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
@@ -43,6 +44,7 @@ card.addEventListener('change', function (event) {
     }
 });
 
+// Handle form submit
 var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function (ev) {
